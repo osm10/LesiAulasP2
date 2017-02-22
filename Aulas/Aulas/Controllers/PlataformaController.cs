6ebf0b3df9d157ce4e29c3d306f9946409b84394
Oliver <Oliver@DESKTOP-H0E0MD2>
2017-02-22 18:41:26 +0000

    Otimizar código.

--- Aulas/Aulas/Controllers/PlataformaController.cs
+++ Aulas/Aulas/Controllers/PlataformaController.cs
@@ -10,10 +10,7 @@ namespace Aulas.Controller
     public class PlataformaController
     {
         private Menu _menuState;
-        //private int _opcao;
-        //float pi = 3.14f;
-
-        //private List<Artigo> artigos;
+        
         ArtigoController ac = new ArtigoController();
         
         public PlataformaController()
@@ -22,36 +19,26 @@ namespace Aulas.Controller
             //_opcao = -1;
         }
 
-        // TODO: 
-        // Hack:
         public void InicializarPlataformaController()
         {
-            float f; //= 3.14f;
-            //int a = f as float;
+            float f; 
+            
 
             while (_menuState != Menu.Sair)
             {
                 Console.Clear();
                 Console.WriteLine("Escolha uma opcao!");
-                Console.WriteLine("O - Para não fazer nada");
                 Console.WriteLine("1 - Para Inserir um Artigo");
                 Console.WriteLine("2 - Para Listar os Artigos");
                 Console.WriteLine("3 - Para Remover um Artigo");
                 Console.WriteLine((int)Menu.Sair+" - Para sair");
                 string opcao = Console.ReadLine();
 
-                if (Menu.TryParse(opcao, out _menuState))
-                {
-                    //_menuState = (Menu) _opcao;
-                    Console.WriteLine("OK");
-                    Console.WriteLine("Prima Qualquer tecla para continuar");
-                    Console.ReadLine();
-                }
-                else
+                if (! Menu.TryParse(opcao, out _menuState))
                 {
                     _menuState = Menu.Invalido;
                     Console.WriteLine("Not OK!");
-                    Console.WriteLine("Prima Qualquer tecla para continuar");
+                    Console.WriteLine("Prima um número relativo à opções existentes!");
                     Console.ReadLine();
                 }
 
@@ -89,26 +76,18 @@ namespace Aulas.Controller
                             break;
 
                         case Menu.RemoverArtigo:
+                        
                             Console.WriteLine("Nome do Artigo");
                             string nome = Console.ReadLine();
-                            if (ac.RemoverArtigo(nome))
+                            while (!ac.RemoverArtigo(nome))
                             {
-                                Console.WriteLine("O artigo foi removido com sucesso!");
+                                Console.WriteLine("Escreva o nome do artigo que esteja na lista!");
+                                nome = Console.ReadLine();
                             }
-                            else
-                            {
-                                while (!ac.RemoverArtigo(nome))
-                                {
-                                    Console.WriteLine("Escreva o nome do artigo que esteja na lista!");
-                                    nome = Console.ReadLine();
-                            }                                
-                            }                         
+                            Console.WriteLine("O artigo foi removido com sucesso!");
                             Console.ReadKey();
                             break;
 
-                        case Menu.Sair:
-                            //_opcao = 0;
-                            break;
                         default:
                             Console.WriteLine("Opção Inválida!");
                             break;
